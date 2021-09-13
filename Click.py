d50c1477deed82408e5e90c113d8bd4d843a8e94
949027 <82878668+949027@users.noreply.github.com>
2021-09-13 11:13:56 +0700

    Update 'Click'

--- Click.py
+++ Click.py
@@ -1,7 +1,7 @@
-import requests
-from urllib.parse import urlparse
-from dotenv import load_dotenv
 import os
+from urllib.parse import urlparse
+import requests
+from dotenv import load_dotenv
 
 
 def shorten_link(token, url):
@@ -16,28 +16,31 @@ def shorten_link(token, url):
 
 def count_clicks(token, url):
     parse = urlparse(url)
-    url_tamplate = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
-    url = url_tamplate.format(parse.netloc + parse.path)
+    url_tamplate = 'https://api-ssl.bitly.com/v4/bitlinks/{}/{}/clicks/summary'
+    url = url_tamplate.format(parse.netloc, parse.path)
     payload = {'units': -1}
     response = requests.get(url, headers={'Authorization': token}, params=payload)
     response.raise_for_status()
     return response.json()['total_clicks']
 
 
-def is_bitlink(url):
+def is_bitlink(token, url):
+    url_tamplate = 'https://api-ssl.bitly.com/v4/bitlinks/{}/{}'
     parse = urlparse(url)
-    return parse.netloc == 'bit.ly'
+    url = url_tamplate.format(parse.netloc, parse.path)
+    response = requests.get(url, headers={'Authorization': token})
+    return response.ok
 
 
 if __name__ == '__main__':
     load_dotenv()
-    token = os.getenv('TOKEN')
+    token = os.getenv('BITLY_TOKEN')
     url = input('Input URL or bitlink\n=> ')
-    if is_bitlink(url):
+    if is_bitlink(token, url):
         try:
             print(count_clicks(token, url))
         except requests.exceptions.HTTPError:
-            print('Incorrect bitlinks')
+            print('Incorrect bitlink')
     else:
         try:
             print('Bitlink:', shorten_link(token, url))
