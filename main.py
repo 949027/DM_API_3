af3ba050e5dae6017246c73e28c4b5333e3a326c
949027 <949027@gmail.com>
2021-09-28 12:08:56 +0700

    Update

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import argparse
+import os
+from urllib.parse import urlparse
+import requests
+from dotenv import load_dotenv
 
 
-# Press the green button in the gutter to run the script.
+def shorten_link(token, url):
+    response = requests.post(
+        'https://api-ssl.bitly.com/v4/shorten',
+        json={'long_url': url},
+        headers={'Authorization': token},
+    )
+    response.raise_for_status()
+    return response.json()['link']
+
+
+def count_clicks(token, url):
+    parse = urlparse(url)
+    url_tamplate = 'https://api-ssl.bitly.com/v4/bitlinks/{}/{}/clicks/summary'
+    url = url_tamplate.format(parse.netloc, parse.path)
+    payload = {'units': -1}
+    response = requests.get(url, headers={'Authorization': token}, params=payload)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(token, url):
+    url_tamplate = 'https://api-ssl.bitly.com/v4/bitlinks/{}/{}'
+    parse = urlparse(url)
+    url = url_tamplate.format(parse.netloc, parse.path)
+    response = requests.get(url, headers={'Authorization': token})
+    return response.ok
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    load_dotenv()
+    token = os.getenv('BITLY_TOKEN')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='URL страницы')
+    url = parser.parse_args().url
+    if is_bitlink(token, url):
+        try:
+            print('Количество переходов по ссылке битли = ', count_clicks(token, url))
+        except requests.exceptions.HTTPError:
+            print('Incorrect bitlink')
+    else:
+        try:
+            print('Bitlink:', shorten_link(token, url))
+        except requests.exceptions.HTTPError:
+            print('Incorrect URL')
