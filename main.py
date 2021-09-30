55aeea7b136cbe5bcc4706999ceeec5d88cfae3a
949027 <949027@gmail.com>
2021-09-30 10:38:01 +0700

    Устранены длинные строки

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ def count_clicks(token, url):
     url_tamplate = 'https://api-ssl.bitly.com/v4/bitlinks/{}/{}/clicks/summary'
     url = url_tamplate.format(parse.netloc, parse.path)
     payload = {'units': -1}
-    response = requests.get(url, headers={'Authorization': token}, params=payload)
+    response = requests.get(
+        url,
+        headers={'Authorization': token},
+        params=payload,
+
+    )
     response.raise_for_status()
     return response.json()['total_clicks']
 
@@ -37,13 +42,20 @@ def is_bitlink(token, url):
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('BITLY_TOKEN')
-    parser = argparse.ArgumentParser(description='При передаче URL страницы в качестве аргумента - создается короткая ссылка (битлинк). '
-                                                 'При передаче битлинка в качестве аргумента - выводится количество переходов по нему.' )
+    parser = argparse.ArgumentParser(
+        description='При передаче URL страницы в качестве аргумента - '
+                    'создается короткая ссылка (битлинк). При передаче '
+                    'битлинка в качестве аргумента - выводится количество '
+                    'переходов по нему.'
+    )
     parser.add_argument('url', help='URL страницы, либо битлинк')
     url = parser.parse_args().url
     if is_bitlink(token, url):
         try:
-            print('Количество переходов по ссылке битли = ', count_clicks(token, url))
+            print(
+                'Количество переходов по ссылке битли = ',
+                count_clicks(token, url)
+            )
         except requests.exceptions.HTTPError:
             print('Incorrect bitlink')
     else:
